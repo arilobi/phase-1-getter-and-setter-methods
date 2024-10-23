7120c00ad0f6399f34d212f033b6a4a4f73bd7f6
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-23 20:47:45 +0300

    add getter and setter

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this._radius = radius;
+  }
+
+  get radius() {
+    return this._radius;
+  }
+
+  set radius(radius) {
+    this._radius = radius;
+  }
+
+  get diameter() {
+    return this._radius * 2;
+  }
+
+  set diameter(diameter) {
+    this._radius = diameter / 2;
+  }
+
+  get circumference() {
+    return Math.PI * this.diameter;
+  }
+
+  set circumference(circumference) {
+    this._radius = circumference / (Math.PI * 2);
+  }
+
+ 
+  get area() {
+    return Math.PI * Math.pow(this._radius, 2);
+  }
+
+
+  set area(area) {
+    this._radius = Math.sqrt(area / Math.PI);
+  }
+}
+
+let circle = new Circle(5); 
+
+console.log(circle.radius);        
+console.log(circle.diameter);       
+console.log(circle.circumference);  
+console.log(circle.area);           
+
+circle.diameter = 20;
+console.log(circle.radius);         
+
+circle.circumference = 31.4159;
+console.log(circle.radius);         
+
+circle.area = 78.5398;
+console.log(circle.radius);         
